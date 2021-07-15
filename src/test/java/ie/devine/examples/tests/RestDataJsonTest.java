8bf2c3c402dc7bf6288ec0b9eebb82342d667e05
Mark Devine <mfmdevine@gmail.com>
2021-07-15 12:07:15 +0100

    Added multithreading example

--- src/test/java/ie/devine/examples/tests/RestDataJsonTest.java
+++ src/test/java/ie/devine/examples/tests/RestDataJsonTest.java
@@ -5,6 +5,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import ie.devine.examples.models.DataModel;
 import ie.devine.examples.models.HeadersModel;
 import io.restassured.response.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.Map;
@@ -15,9 +21,11 @@ import static org.apache.http.HttpStatus.SC_OK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
+@Slf4j
 public class RestDataJsonTest extends RestDataBase {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
+    int nThreads = 10;
 
     @Test
     public void generateDifferentRequestBodyEveryTime() {
@@ -50,5 +58,44 @@ public class RestDataJsonTest extends RestDataBase {
             String id = response.getBody().jsonPath().get(ID_PATH);
             assertThat(id).isEqualTo(dataModel.getId());
         });
+    }
+
+    @Test
+    public void multiThreadRequests() {
+        ArrayList<String> responseList = new ArrayList<>();
+        ExecutorService exec = Executors.newFixedThreadPool(nThreads);
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            exec.submit(() -> {
+                String response = executeServiceCall(finalI);
+                responseList.add(response);
+            });
+        } exec.shutdown();
+        try {
+            exec.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            log.error(e.toString());
+        }
+        log.info("Number of requests made " + responseList.size());
+        for(String entry: responseList) {
+            log.info("Entries: " + entry);
+        }
+    }
+
+    private String executeServiceCall(int number) {
+        DataModel dataModel = DataModel.builder().build();
+        Map<String, String> headersFromModel = objectMapper.convertValue(
+            HeadersModel.builder().build(), new TypeReference<Map<String, String>>() {
+            });
+
+        Response response = given()
+            .spec(requestSpecification)
+            .basePath(BASE_PATH)
+            .when()
+                .headers(headersFromModel)
+                .body(dataModel)
+                .post();
+        return response.getStatusCode() == 200 ? "Success: " + dataModel.toString() : "Failure: " + response
+            .getStatusCode() + " : " + dataModel.toString();
     }
 }
